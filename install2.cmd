7bcb751f3540b3a1c192def55062f86f703e8dcd
xfmxx <78693466+xfmxx@users.noreply.github.com>
2023-04-27 23:41:49 +0200

    Update install2.cmd

--- install2.cmd
+++ install2.cmd
@@ -10,7 +10,7 @@ REM Call add.php API to add the folder to the server
 curl %serverip%/add.php?folder=%name%
 
 REM Create a new scheduled task to run Chrome in kiosk mode on startup
-schtasks /create /tn "Chromium Kiosk" /tr "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe --disable-infobars --kiosk %serverip%/%name%" /sc onstart /ru %USERNAME%
+schtasks /create /tn "Chromium Kiosk" /tr "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe --disable-infobars --kiosk %serverip%/%name%" /sc onstart /ru "%USERNAME%"
 
 REM Reboot the system to start Chrome in kiosk mode
 shutdown /r /t 0
